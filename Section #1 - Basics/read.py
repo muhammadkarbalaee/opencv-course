5237f043a704d5c0d666c089240160190474736d
Muhammad Karbalaee <77221151+muhammadksht@users.noreply.github.com>
2022-09-22 11:57:44 +0330

    learnt reading and resizing images

--- Section #1 - Basics/read.py
+++ Section #1 - Basics/read.py
@@ -1,27 +1,15 @@
-#pylint:disable=no-member
-
 import cv2 as cv
 
-img = cv.imread('../Resources/Photos/cats.jpg')
-cv.imshow('Cats', img)
+# image = cv.imread('../Resources/Photos/cat_large.jpg')
+# cv.imshow("cat",image)
 
-cv.waitKey(0)
-
-# Reading Videos
-capture = cv.VideoCapture('../Resources/Videos/dog.mp4')
+capture = cv.VideoCapture("../Resources/Videos/dog.mp4")
 
 while True:
     isTrue, frame = capture.read()
-    
-    # if cv.waitKey(20) & 0xFF==ord('d'):
-    # This is the preferred way - if `isTrue` is false (the frame could 
-    # not be read, or we're at the end of the video), we immediately
-    # break from the loop. 
-    if isTrue:    
-        cv.imshow('Video', frame)
-        if cv.waitKey(20) & 0xFF==ord('d'):
-            break            
-    else:
+    cv.imshow("yes",frame)
+
+    if cv.waitKey(20) & 0xFF == ord("d"):
         break
 
 capture.release()
---
+++ Section #1 - Basics/rescale.py
@@ -0,0 +1,19 @@
+import cv2 as cv
+
+
+def rescaleFrame(frame, scale=0.75):
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+
+    dimensions = (width,height)
+
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+
+
+image = cv.imread('../Resources/Photos/cat.jpg')
+
+resized = rescaleFrame(image,0.5)
+
+cv.imshow("",resized)
+
+cv.waitKey(0)
